babbc8d409021656f520cf5862f97371cfc5f616
richardkelvin81 <richard.aparicio@gmail.com>
2024-08-17 23:31:27 -0400

    ACTUALIZADO PARA CORS

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,33 @@
-// Después (ES6)
 import express from 'express';
 import cors from 'cors';
 import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from "@bot-whatsapp/bot";
 import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys";
 
-const app = express();
 const flowBienvenida = addKeyword('hola').addAnswer('¡Cómo estás!, bienvenido a TAXI CORP');
 
-app.use(cors()); // Habilitar CORS para todas las rutas
+const main = async () => {
+    const provider = createProvider(BaileysProvider);
+    provider.initHttpServer(3001);
 
-app.post('/enviar-whatsapp', (req, res) => {
-    handleCtx(async (bot, req, res) => {
-        const phone = req.body.phone;
-        const message = req.body.message;
-        await bot.sendMessage(phone, message, {});
-        res.json({ message: 'Mensaje enviado desde servidor DonWeb' });
-    })(req, res);
-});
+    const app = express();
 
-const provider = createProvider(BaileysProvider);
-provider.initHttpServer(3001);
+    // Habilitar CORS para todas las rutas
+    app.use(cors());
 
-createBot({
-    flow: createFlow([flowBienvenida]),
-    database: new MemoryDB(),
-    provider
-});
+    provider.http?.server.post('/enviar-whatsapp', (req, res) => {
+        handleCtx(async (bot, req, res) => {
+            const phone = req.body.phone;
+            const message = req.body.message;
+            await bot.sendMessage(phone, message, {});
+            res.end('Mensaje enviado desde servidor DonWeb');
+        })(req, res);
+    });
+
+    await createBot({
+        flow: createFlow([flowBienvenida]),
+        database: new MemoryDB(),
+        provider
+    });
+}
+
+main();
